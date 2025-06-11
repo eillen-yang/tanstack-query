287053a182d0e5e1de0e9e57512f5a764c26dc41
양아름 <yangareum0130@gmail.com>
2025-06-11 15:31:02 +0900

    feat: setting 완료

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1>Hi ~ 😜</h1>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
