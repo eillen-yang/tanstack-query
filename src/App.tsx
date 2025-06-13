5edb0497ee7959f13c08294874a7939df76b4b3a
양아름 <yangareum0130@gmail.com>
2025-06-13 17:45:41 +0900

    feat: routeing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/route";
 
 const queryClient = new QueryClient();
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Hi ~ 😜</h1>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 }
---
+++ src/pages/CreatePostPage.tsx
@@ -0,0 +1,3 @@
+export default function CreatePostPage() {
+  return <div className="p-4 text-2xl">게시글 생성 페이지</div>;
+}
