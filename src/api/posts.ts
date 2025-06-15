fecbe3c8abfdd94b2ea565f94d78d6df6004ce9d
양아름 <yangareum0130@gmail.com>
2025-06-15 16:04:00 +0900

    feat: data type정의

---
+++ src/api/posts.ts
@@ -0,0 +1,10 @@
+import type { Post } from "../types/posts";
+import axios from "axios";
+
+export const fetchPosts = async ({ pageParam = 1 }): Promise<Post[]> => {
+  const res = await axios.get<Post[]>(
+    `https://jsonplaceholder.typicode.com/posts?_page=${pageParam}&_limit=10`
+  );
+
+  return res.data;
+};
