dd18e26fd76a764cd25b372c2d7277f638c37c0e
양아름 <yangareum0130@gmail.com>
2025-06-15 17:01:47 +0900

    chore: 게시글 조회 type지정

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -4,3 +4,34 @@ export interface Post {
   body: string;
   userId: number;
 }
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
